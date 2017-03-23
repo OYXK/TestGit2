c24cc280a504295c5841de8d38178a2e27062a0c
2051918090@qq.com <2051918090@qq.com>
2017-03-23 15:31:32 +0800

    111111111

---
+++ Test2/Test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test2
+//
+//  Created by 欧杨 on 17/3/23.
+//  Copyright © 2017年 欧杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test2/Test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test2
+//
+//  Created by 欧杨 on 17/3/23.
+//  Copyright © 2017年 欧杨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
